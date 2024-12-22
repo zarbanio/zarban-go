ced72cc48f1bb604d1f204006620c7be5c455adb
Arash Alaei <arashalaei@aut.ac.ir>
2024-12-22 06:56:39 +0330

    add create child example

--- main.go
+++ main.go
@@ -3,6 +3,5 @@ package main
 import "github.com/zarbanio/zarban-go/examples"
 
 func main() {
-	// Call the SignupExample function from the example package
-	examples.LoginExample()
+	examples.CreateChildExample()
 }
